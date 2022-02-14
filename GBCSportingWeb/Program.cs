8e89fc9fbf44b6b7ddadb06898fefd1a97201648
Stanley Tsonev <stanleytsonev@hotmail.com>
2022-02-14 17:14:26 -0500

    add routing config for urls in program.cs

--- GBCSportingWeb/Program.cs
+++ GBCSportingWeb/Program.cs
@@ -5,6 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddRouting(options =>
+{
+    options.AppendTrailingSlash = true;
+    options.LowercaseUrls = true;
+});
+
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")));
 
