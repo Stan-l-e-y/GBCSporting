61b03c72b2b02b0ddb543650badb8360c1085f0f
sarah7sami <sarah7sami@gmail.com>
2022-02-16 00:06:19 -0500

    Add Customer and Country Models

--- GBCSportingWeb/Models/Customer.cs
+++ GBCSportingWeb/Models/Customer.cs
@@ -6,5 +6,25 @@ namespace GBCSportingWeb.Models
     {
         [Key]
         public int CustomerId { get; set; }
+        [Required(ErrorMessage = "Please enter your first name.")]
+        public string? FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter your last name.")]
+        public string? LastName { get; set; }
+        [Required(ErrorMessage = "Please enter your address.")]
+        public string? Address { get; set; }
+        [Required(ErrorMessage = "Please enter your city.")]
+        public string? City { get; set; }
+        [Required(ErrorMessage = "Please enter your state.")]
+        public string? State { get; set; }
+        [Required(ErrorMessage = "Please enter your postal code.")]
+        public string? PostalCode { get; set; }
+        [Required(ErrorMessage = "Please choose a country.")]
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+
+        // The Email and Phone fields are optional
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+
     }
 }
