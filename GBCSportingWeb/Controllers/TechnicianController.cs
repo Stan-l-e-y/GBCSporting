665fac378b92856c7539aeca1151b759180dd1e9
FJalaliAmeri <f.jalali.ameri@gmail.com>
2022-02-16 00:10:13 -0500

    Add Technician Controller Edit

--- GBCSportingWeb/Controllers/TechnicianController.cs
+++ GBCSportingWeb/Controllers/TechnicianController.cs
@@ -34,6 +34,40 @@ namespace GBCSportingWeb.Controllers
             var technician = context.Technicians.Find(id);
             return View(technician);
         }
+
+        [HttpPost]
+        public IActionResult Edit(Technician technician)
+        {
+            if (ModelState.IsValid)
+            {
+                if (technician.TechnicianId == 0)
+                    context.Technicians.Add(technician);
+                else
+                    context.Technicians.Update(technician);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Action = (technician.TechnicianId == 0) ? "Add" : "Edit";
+                return View(technician);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var technician = context.Technicians.Find(id);
+            return View(technician);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Technician technician)
+        {
+            context.Technicians.Remove(technician);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
     }
 
 }
