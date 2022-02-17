be0353b3874bcda0d5f09455662cbd83442330f3
sarah7sami <sarah7sami@gmail.com>
2022-02-17 00:33:05 -0500

    Update Customer Controller

--- GBCSportingWeb/Controllers/CustomerController.cs
+++ GBCSportingWeb/Controllers/CustomerController.cs
@@ -15,25 +15,10 @@ namespace GBCSportingWeb.Controllers
         public IActionResult Index()  //TODO wtf is the error here, I'm logging off bye
         {
             var customers = context.Customers
-                            .Include(c => c.FirstName)
-                            .Include(c => c.LastName)
-                            .Include(c => c.Email)
-                            .Include(c => c.City)
                             .OrderBy(c => c.FirstName).ThenBy(c => c.LastName)
                             .ToList();
 
             return View(customers);
         }
-
-        //[HttpGet]
-        //public IActionResult Create()
-        //{
-        //    ViewBag.Action = "Add";
-        //    ViewBag.Products = context.Products.OrderBy(p => p.ProductId).ToList();
-        //    ViewBag.Customers = context.Customers.OrderBy(c => c.CustomerId).ToList();
-        //    ViewBag.Technicians = context.Technicians.OrderBy(t => t.TechnicianId).ToList();
-
-        //    return View("Edit", new Customer());
-        //}
     }
 }
