6cdcbae1687623e732bc37dcb1646b380e5c4a7c
sarah7sami <sarah7sami@gmail.com>
2022-02-24 00:20:47 -0500

    Minor Changes

---
+++ GBCSportingWeb/Migrations/20220224042655_CustomerModelChanges.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GBCSportingWeb.Migrations
+{
+    public partial class CustomerModelChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "Address", "City", "CountryId", "Email", "FirstName", "LastName", "Phone", "PostalCode", "State" },
+                values: new object[] { 2, "4th Avenue", "NYC", 187, null, "Stanley", "Tsonev", null, "J1C 8Y1", "New York" });
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentId",
+                keyValue: 1,
+                columns: new[] { "DateClosed", "DateOpened" },
+                values: new object[] { new DateTime(2022, 2, 23, 23, 26, 55, 2, DateTimeKind.Local).AddTicks(6127), new DateTime(2022, 2, 23, 23, 26, 55, 2, DateTimeKind.Local).AddTicks(6123) });
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentId",
+                keyValue: 2,
+                columns: new[] { "DateClosed", "DateOpened" },
+                values: new object[] { new DateTime(2022, 2, 23, 23, 26, 55, 2, DateTimeKind.Local).AddTicks(6132), new DateTime(2022, 2, 23, 23, 26, 55, 2, DateTimeKind.Local).AddTicks(6130) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "ReleaseDate",
+                value: new DateTime(2022, 2, 23, 23, 26, 55, 2, DateTimeKind.Local).AddTicks(6048));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "ReleaseDate",
+                value: new DateTime(2022, 2, 23, 23, 26, 55, 2, DateTimeKind.Local).AddTicks(6083));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentId",
+                keyValue: 1,
+                columns: new[] { "DateClosed", "DateOpened" },
+                values: new object[] { new DateTime(2022, 2, 22, 17, 22, 32, 63, DateTimeKind.Local).AddTicks(125), new DateTime(2022, 2, 22, 17, 22, 32, 63, DateTimeKind.Local).AddTicks(123) });
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentId",
+                keyValue: 2,
+                columns: new[] { "DateClosed", "DateOpened" },
+                values: new object[] { new DateTime(2022, 2, 22, 17, 22, 32, 63, DateTimeKind.Local).AddTicks(126), new DateTime(2022, 2, 22, 17, 22, 32, 63, DateTimeKind.Local).AddTicks(126) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "ReleaseDate",
+                value: new DateTime(2022, 2, 22, 17, 22, 32, 63, DateTimeKind.Local).AddTicks(81));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "ReleaseDate",
+                value: new DateTime(2022, 2, 22, 17, 22, 32, 63, DateTimeKind.Local).AddTicks(103));
+        }
+    }
+}
---
+++ GBCSportingWeb/Migrations/20220224043453_SeedUpdate.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GBCSportingWeb.Migrations
+{
+    public partial class SeedUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 2,
+                column: "Email",
+                value: "Stanley.Tsonev@gmail.com");
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "Address", "City", "CountryId", "Email", "FirstName", "LastName", "Phone", "PostalCode", "State" },
+                values: new object[] { 3, "7th Avenue", "Tehran", 80, "Farshad.Jalali@gmail.com", "Farshad", "Jalali", null, "k1C 9T1", "Tehran" });
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentId",
+                keyValue: 1,
+                columns: new[] { "DateClosed", "DateOpened" },
+                values: new object[] { new DateTime(2022, 2, 23, 23, 34, 52, 918, DateTimeKind.Local).AddTicks(9212), new DateTime(2022, 2, 23, 23, 34, 52, 918, DateTimeKind.Local).AddTicks(9208) });
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentId",
+                keyValue: 2,
+                columns: new[] { "DateClosed", "DateOpened" },
+                values: new object[] { new DateTime(2022, 2, 23, 23, 34, 52, 918, DateTimeKind.Local).AddTicks(9217), new DateTime(2022, 2, 23, 23, 34, 52, 918, DateTimeKind.Local).AddTicks(9215) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "ReleaseDate",
+                value: new DateTime(2022, 2, 23, 23, 34, 52, 918, DateTimeKind.Local).AddTicks(9132));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "ReleaseDate",
+                value: new DateTime(2022, 2, 23, 23, 34, 52, 918, DateTimeKind.Local).AddTicks(9164));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 2,
+                column: "Email",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentId",
+                keyValue: 1,
+                columns: new[] { "DateClosed", "DateOpened" },
+                values: new object[] { new DateTime(2022, 2, 23, 23, 26, 55, 2, DateTimeKind.Local).AddTicks(6127), new DateTime(2022, 2, 23, 23, 26, 55, 2, DateTimeKind.Local).AddTicks(6123) });
+
+            migrationBuilder.UpdateData(
+                table: "Incidents",
+                keyColumn: "IncidentId",
+                keyValue: 2,
+                columns: new[] { "DateClosed", "DateOpened" },
+                values: new object[] { new DateTime(2022, 2, 23, 23, 26, 55, 2, DateTimeKind.Local).AddTicks(6132), new DateTime(2022, 2, 23, 23, 26, 55, 2, DateTimeKind.Local).AddTicks(6130) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "ReleaseDate",
+                value: new DateTime(2022, 2, 23, 23, 26, 55, 2, DateTimeKind.Local).AddTicks(6048));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "ReleaseDate",
+                value: new DateTime(2022, 2, 23, 23, 26, 55, 2, DateTimeKind.Local).AddTicks(6083));
+        }
+    }
+}
